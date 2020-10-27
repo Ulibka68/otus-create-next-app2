eeb71f4669e158eb7463a8f4e8145a5d875239d6
Gayrat <gena68@inbox.ru>
2020-10-27 12:44:11 +0300

    добавлен responsive-loader изображения работают

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import responsiveImage from "../images/4mb-image.jpg?sizes[]=300,sizes[]=600,sizes[]=1024";
+// import responsiveImage2 from "images/4mb-image.jpg?webp";
+// import responsiveImage from "images/4mb-image.jpg?sizes[]=300,sizes[]=600,sizes[]=1024&format=webp";
+// import responsiveImage from "images/4mb-image.jpg";
+// import responsiveImage from "img/myImage.jpg?sizes[]=300,sizes[]=600,sizes[]=1024,sizes[]=2048";
+// import responsiveImageWebp from 'img/myImage.jpg?sizes[]=300,sizes[]=600,sizes[]=1024,sizes[]=2048&format=webp';
+
 
 export default function Home() {
   return (
@@ -10,44 +17,17 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1>Тестирование изображений</h1>
+        <picture>
+          <source srcSet={responsiveImage.srcSet} />
+          <img
+              src={responsiveImage.src}
+              srcSet={responsiveImage.srcSet}
+              width={responsiveImage.width}
+              height={responsiveImage.height}
+              loading="lazy"
+          />
+        </picture>
       </main>
 
       <footer className={styles.footer}>
